32a7e378222a8cbae40423a19daa578d22855940
ranashahid43 <118065248+ranashahid43@users.noreply.github.com>
2026-01-10 14:29:28 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-  <title>{{ $title ?? 'The Testing Tech' }}</title>
+  <title>{{ $title ?? 'AuraMetric Soloution' }}</title>
 
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 
